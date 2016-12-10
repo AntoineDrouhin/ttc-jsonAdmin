f16938040b1c9c6059dd96b7b7f251919a42aed2
Aurélien Garret <aurelien.garret@gmail.com>
2016-12-10 14:59:10 +0100

    with errors

--- examples/exampleCtrl.js
+++ examples/exampleCtrl.js
@@ -1,5 +1,5 @@
 // Define the `phonecatApp` module
-var example = angular.module('example', ['ttcJsonAdmin']);
+var example = angular.module('example', ['ngRoute', 'ttcJsonAdminModule']);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 example.controller('ExampleCtrl', function ExampleCtrl($scope) {
--- src/jsonAdminDirective.js
+++ src/jsonAdminDirective.js
@@ -1,9 +1,10 @@
 'use strict';
 
-angular.module('ttcJsonAdmin', ['ngMaterial'])
+angular.module('ttcJsonAdminModule', ['ngMaterial'])
     .directive('ttcJsonAdmin', ['$log', '$http', '$mdDialog', function($log,
         $http, $mdDialog) {
         return {
+            restrict : 'E',
             templateUrl: './jsonAdminView.html',
             scope: {
                 'routeApiActions': '=routeApiActions'
