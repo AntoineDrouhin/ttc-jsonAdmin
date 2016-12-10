f9be584e21f7c9e033d4ea94c633ca25ab39a9b8
Aurélien Garret <aurelien.garret@gmail.com>
2016-12-10 15:46:45 +0100

    Fix and isolate this component

--- examples/exampleCtrl.js
+++ examples/exampleCtrl.js
@@ -1,5 +1,5 @@
 // Define the `phonecatApp` module
-var example = angular.module('example', ['ngRoute', 'ttcJsonAdminModule']);
+var example = angular.module('example', ['ttcJsonAdminModule']);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 example.controller('ExampleCtrl', function ExampleCtrl($scope) {
